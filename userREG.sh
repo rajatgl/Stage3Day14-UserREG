43952007922528db5c1b245f882b46bfe391f163
Rajat Gundi <glrajat@gmail.com>
2020-08-15 21:43:41 +0530

    Email Validation

--- userREG.sh
+++ userREG.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-echo "Welcome To User Registration Prototype"
+echo $'Welcome To User Registration Prototype\n'
 
 #UC1-----First name validation
 
@@ -30,3 +30,15 @@ else
         echo "Invalid Format"
 fi
 
+read -p "Email address : " EMAIL
+
+#Email Pattern
+EMAIL_PATTERN="^[a-z0-9]{3,15}(|[.]?[a-z0-9]+)@[a-z0-9]{1,15}(.[a-z]{2,4})(|[.]?[a-z]{2,4})$"
+
+#Email Validation
+if [[ $EMAIL =~ $EMAIL_PATTERN ]]
+then
+        echo "Valid Format"
+else
+        echo "Invalid Format"
+fi
