beee23275e386412ad559d9dea73122d70585bb6
Rajat Gundi <glrajat@gmail.com>
2020-08-15 18:11:42 +0530

    First Name Validation

--- userREG.sh
+++ userREG.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "Welcome To User Registration Prototype"
+
+#UC1-----First name validation
+
+read -p "First Name : " FIRSTNAME
+
+FIRST_NAME_PATTERN="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+if [[ $FIRSTNAME =~ $FIRST_NAME_PATTERN ]]
+then
+	echo "Valid"
+else
+	echo "Invalid"
+fi
