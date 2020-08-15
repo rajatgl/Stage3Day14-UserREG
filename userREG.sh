06212514f3b9ed8be5ca9ff5e3b4864d7ceb2a23
Rajat Gundi <glrajat@gmail.com>
2020-08-15 18:31:17 +0530

    Last Name Validation

--- userREG.sh
+++ userREG.sh
@@ -6,11 +6,27 @@ echo "Welcome To User Registration Prototype"
 
 read -p "First Name : " FIRSTNAME
 
+#First Name Pattern
 FIRST_NAME_PATTERN="^[[:upper:]]{1}[[:lower:]]{2,}$"
 
+#First Name Validation
 if [[ $FIRSTNAME =~ $FIRST_NAME_PATTERN ]]
 then
-	echo "Valid"
+	echo "Valid Format"
 else
-	echo "Invalid"
+	echo "Invalid Format"
 fi
+
+read -p "Last Name : " LASTNAME
+
+#Last Name Pattern
+LAST_NAME_PATTERN="^[[:upper:]]{1}[[:lower:]]{2,}$"
+
+#Last Name Validation
+if [[ $LASTNAME =~ $LAST_NAME_PATTERN ]]
+then
+        echo "Valid Format"
+else
+        echo "Invalid Format"
+fi
+
